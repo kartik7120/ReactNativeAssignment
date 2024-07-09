8686628331d043332bfa845b1b1757e233d708c4
Kaartik Shukla <kaartikshukla7120@gmail.com>
2024-07-09 22:25:03 +0530

    Created ui for pizza detail page

--- app/pizza/[id].tsx
+++ app/pizza/[id].tsx
@@ -1,21 +1,36 @@
 import { View, Text } from 'react-native'
 import { useLocalSearchParams, useNavigation } from 'expo-router';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Pizza } from '../(tabs)';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Pizza() {
+export default function PizzaComponent() {
 
-    const { id, pizzaName } = useLocalSearchParams();
+    const { id, name, price, toppings, rank } = useLocalSearchParams();
     const navigation = useNavigation();
 
     useEffect(() => {
         navigation.setOptions({
-            title: `${pizzaName}`
+            title: `${name}`
         })
-    })
+    }, [id])
+
+    console.log(toppings)
 
     return (
-        <View>
-            <Text>{id}</Text>
-        </View>
+        <SafeAreaView style={{ height: "auto" }}>
+            <View style={{ padding: 20 }}>
+                <Text style={{ fontSize: 30, color: "white" }}>{name}</Text>
+                <View style={{ flexDirection: "row", gap: 5 }}>
+                    <Text style={{ fontSize: 20, color: "white" }}>Toppings: </Text>
+                    <View style={{ flexDirection: "row", gap: 10 }}>
+                        {toppings && typeof toppings === 'string' && JSON.parse(toppings).map((topping: string, index: number) => (
+                            <Text key={index} style={{ fontSize: 20, color: "white" }}>{topping + `${index < topping.length - 1 ? "," : ""}`}</Text>
+                        ))}
+                    </View>
+                </View>
+                <Text style={{ color: "white", fontSize: 20 }}>Price : {"$" + price}</Text>
+            </View>
+        </SafeAreaView>
     )
 }
