e90be01bb5e7a7906ad428603d6db4027198950c
Kaartik Shukla <kaartikshukla7120@gmail.com>
2024-07-09 19:02:12 +0530

    added cart tab on the bottom tab navigator

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,9 +26,9 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'Cart',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'cart' : 'cart-outline'} color={color} />
           ),
         }}
       />
